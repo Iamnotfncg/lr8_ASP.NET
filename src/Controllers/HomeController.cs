01c13c44277a917050e834f0d79c312dfc65e94c
Iamnotfncg <64169223+Iamnotfncg@users.noreply.github.com>
2023-12-17 21:07:56 +0200

    first

---
+++ src/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace lr9.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            var products = new List<Product>()
+            {
+                new() { Id = 1, Name = "Product1", Price = 10.99m },
+                new() { Id = 2, Name = "Product2", Price = 20.49m },
+                new() { Id = 3, Name = "Product3", Price = 15.79m },
+                new() { Id = 4, Name = "Product4", Price = 5.99m },
+                new() { Id = 5, Name = "Product5", Price = 8.50m },
+                new() { Id = 6, Name = "Product6", Price = 12.25m }
+            };
+
+            ViewBag.City = "Paris";
+
+            return View(products);
+        }
+    }
+}
---
+++ src/ViewComponents/WeatherViewComponent.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace lr9.ViewComponents
+{
+    public class WeatherViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(string city)
+        {
+            var apiKey = "0d99ca06941bfa04d9b9fb925fa818d9";
+            var apiUrl = $"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}";
+            WeatherResponse? response;
+            using (var client = new HttpClient())
+            {
+                response = await client.GetFromJsonAsync<WeatherResponse>(apiUrl);
+            }
+            return View(response);
+        }
+    }
+
+    public class WeatherResponse
+    {
+        public MainInfo Main { get; set; }
+        public WeatherInfo[] Weather { get; set; }
+    }
+
+    public class MainInfo
+    {
+        public float Temp { get; set; }
+    }
+
+    public class WeatherInfo
+    {
+        public string Description { get; set; }
+    }
+}
